b330f32e2e8810880ba08f2baba64427dbb10adb
Adria <adriagp90@gmail.com>
2023-12-15 20:23:45 +0100

    afegides rutes de CentreController

--- practica4/my-project/routes/admin.php
+++ practica4/my-project/routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CentresController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 
@@ -14,6 +15,20 @@ use App\Http\Controllers\Admin\AdminController;
         Route::get('/alumnat', [AdminController::class, 'alumnat'])->name('alumnat');
 
         Route::get('/professorat', [AdminController::class, 'professorat'])->name('professorat');
+
+        Route::get('/indexCentre', [CentresController::class, 'index'])->name('indexCentres');
+
+        Route::get('/createCentres', [CentresController::class, 'create'])->name('createCentre');
+
+        Route::post('/storeCentres', [CentresController::class, 'store'])->name('storeCentre');
+
+        Route::get('/showCentre', [CentresController::class, 'show'])->name('showCentre');
+
+        Route::get('/editCentre/{id}', [CentresController::class, 'edit'])->name('editCentre');
+
+        Route::put('/updateCentre', [CentresController::class, 'update'])->name('updateCentre');
+
+        Route::get('/destroyCentre/{id}', [CentresController::class, 'destroy'])->name('destroyCentre');
     });
 
 
